21637d8f58126771c31b53fb44feabf1b29c3359
Preston Ford <preston.y.ford@gmail.com>
2025-02-01 20:06:53 -0700

    StatusItemScroller component

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -12,10 +12,9 @@ import Login from "./components/authentication/login/Login";
 import Register from "./components/authentication/register/Register";
 import MainLayout from "./components/mainLayout/MainLayout";
 import Toaster from "./components/toaster/Toaster";
-import FeedScroller from "./components/mainLayout/FeedScroller";
-import StoryScroller from "./components/mainLayout/StoryScroller";
-import { AuthToken, User, FakeData } from "tweeter-shared";
+import { AuthToken, User, FakeData, Status } from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
+import StatusItemScroller from "./components/mainLayout/StatusItemScroller";
 
 const App = () => {
   const { currentUser, authToken } = useContext(UserInfoContext);
@@ -39,6 +38,26 @@ const App = () => {
 };
 
 const AuthenticatedRoutes = () => {
+  const loadMoreFeedItems = async (
+    authToken: AuthToken,
+    userAlias: string,
+    pageSize: number,
+    lastItem: Status | null
+  ): Promise<[Status[], boolean]> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+  };
+
+  const loadMoreStoryItems = async (
+    authToken: AuthToken,
+    userAlias: string,
+    pageSize: number,
+    lastItem: Status | null
+  ): Promise<[Status[], boolean]> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+  };
+
   const loadMoreFollowers = async (
     authToken: AuthToken,
     userAlias: string,
@@ -63,8 +82,18 @@ const AuthenticatedRoutes = () => {
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<Navigate to="/feed" />} />
-        <Route path="feed" element={<FeedScroller />} />
-        <Route path="story" element={<StoryScroller />} />
+        <Route
+          path="feed"
+          element={
+            <StatusItemScroller loadItems={loadMoreFeedItems} itemDescription="feed" />
+          }
+        />
+        <Route
+          path="story"
+          element={
+            <StatusItemScroller loadItems={loadMoreStoryItems} itemDescription="story" />
+          }
+        />
         <Route
           path="followees"
           element={
--- tweeter-web/src/components/mainLayout/StatusItemScroller.tsx
+++ tweeter-web/src/components/mainLayout/StatusItemScroller.tsx
@@ -8,7 +8,17 @@ import StatusItem from "../statusItem/StatusItem";
 
 export const PAGE_SIZE = 10;
 
-const StoryScroller = () => {
+interface Props {
+  loadItems: (
+    authToken: AuthToken,
+    userAlias: string,
+    pageSize: number,
+    lastItem: Status | null
+  ) => Promise<[Status[], boolean]>;
+  itemDescription: string;
+}
+
+const StatusItemScroller = (props: Props) => {
   const { displayErrorMessage } = useToastListener();
   const [items, setItems] = useState<Status[]>([]);
   const [newItems, setNewItems] = useState<Status[]>([]);
@@ -18,7 +28,7 @@ const StoryScroller = () => {
 
   const addItems = (newItems: Status[]) =>
     setNewItems(newItems);
-
+  
   const { displayedUser, authToken } =
     useContext(UserInfoContext);
 
@@ -51,7 +61,7 @@ const StoryScroller = () => {
 
   const loadMoreItems = async () => {
     try {
-      const [newItems, hasMore] = await loadMoreStoryItems(
+      const [newItems, hasMore] = await props.loadItems(
         authToken!,
         displayedUser!.alias,
         PAGE_SIZE,
@@ -64,19 +74,9 @@ const StoryScroller = () => {
       setChangedDisplayedUser(false)
     } catch (error) {
       displayErrorMessage(
-        `Failed to load story items because of exception: ${error}`
+        `Failed to load ${props.itemDescription} items because of exception: ${error}`
       );
     }
-  };
-
-  const loadMoreStoryItems = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: Status | null
-  ): Promise<[Status[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
   };
 
   return (
@@ -99,4 +99,4 @@ const StoryScroller = () => {
   );
 };
 
-export default StoryScroller;
+export default StatusItemScroller;
