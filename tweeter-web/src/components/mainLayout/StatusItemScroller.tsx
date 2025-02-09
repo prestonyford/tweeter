ada1abb6e06f2c505b933bf2fe0c1e35956c8dbe
Preston Ford <preston.y.ford@gmail.com>
2025-02-08 20:47:42 -0700

    whitespace

--- tweeter-web/src/components/mainLayout/StatusItemScroller.tsx
+++ tweeter-web/src/components/mainLayout/StatusItemScroller.tsx
@@ -8,93 +8,93 @@ import useUserInfo from "../userInfo/userInfoHook";
 export const PAGE_SIZE = 10;
 
 interface Props {
-  loadItems: (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: Status | null
-  ) => Promise<[Status[], boolean]>;
-  itemDescription: string;
+	loadItems: (
+		authToken: AuthToken,
+		userAlias: string,
+		pageSize: number,
+		lastItem: Status | null
+	) => Promise<[Status[], boolean]>;
+	itemDescription: string;
 }
 
 const StatusItemScroller = (props: Props) => {
-  const { displayErrorMessage } = useToastListener();
-  const [items, setItems] = useState<Status[]>([]);
-  const [newItems, setNewItems] = useState<Status[]>([]);
-  const [hasMoreItems, setHasMoreItems] = useState(true);
-  const [lastItem, setLastItem] = useState<Status | null>(null);
-  const [changedDisplayedUser, setChangedDisplayedUser] = useState(true);
+	const { displayErrorMessage } = useToastListener();
+	const [items, setItems] = useState<Status[]>([]);
+	const [newItems, setNewItems] = useState<Status[]>([]);
+	const [hasMoreItems, setHasMoreItems] = useState(true);
+	const [lastItem, setLastItem] = useState<Status | null>(null);
+	const [changedDisplayedUser, setChangedDisplayedUser] = useState(true);
 
-  const addItems = (newItems: Status[]) =>
-    setNewItems(newItems);
-  
-  const { displayedUser, authToken } = useUserInfo();
+	const addItems = (newItems: Status[]) =>
+		setNewItems(newItems);
 
-  // Initialize the component whenever the displayed user changes
-  useEffect(() => {
-    reset();
-  }, [displayedUser]);
+	const { displayedUser, authToken } = useUserInfo();
 
-  // Load initial items whenever the displayed user changes. Done in a separate useEffect hook so the changes from reset will be visible.
-  useEffect(() => {
-    if(changedDisplayedUser) {
-      loadMoreItems();
-    }
-  }, [changedDisplayedUser]);
+	// Initialize the component whenever the displayed user changes
+	useEffect(() => {
+		reset();
+	}, [displayedUser]);
 
-  // Add new items whenever there are new items to add
-  useEffect(() => {
-    if(newItems) {
-      setItems([...items, ...newItems]);
-    }
-  }, [newItems])
+	// Load initial items whenever the displayed user changes. Done in a separate useEffect hook so the changes from reset will be visible.
+	useEffect(() => {
+		if (changedDisplayedUser) {
+			loadMoreItems();
+		}
+	}, [changedDisplayedUser]);
 
-  const reset = async () => {
-    setItems([]);
-    setNewItems([]);
-    setLastItem(null);
-    setHasMoreItems(true);
-    setChangedDisplayedUser(true);
-  }
+	// Add new items whenever there are new items to add
+	useEffect(() => {
+		if (newItems) {
+			setItems([...items, ...newItems]);
+		}
+	}, [newItems])
 
-  const loadMoreItems = async () => {
-    try {
-      const [newItems, hasMore] = await props.loadItems(
-        authToken!,
-        displayedUser!.alias,
-        PAGE_SIZE,
-        lastItem
-      );
+	const reset = async () => {
+		setItems([]);
+		setNewItems([]);
+		setLastItem(null);
+		setHasMoreItems(true);
+		setChangedDisplayedUser(true);
+	}
 
-      setHasMoreItems(hasMore);
-      setLastItem(newItems[newItems.length - 1]);
-      addItems(newItems);
-      setChangedDisplayedUser(false)
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to load ${props.itemDescription} items because of exception: ${error}`
-      );
-    }
-  };
+	const loadMoreItems = async () => {
+		try {
+			const [newItems, hasMore] = await props.loadItems(
+				authToken!,
+				displayedUser!.alias,
+				PAGE_SIZE,
+				lastItem
+			);
 
-  return (
-    <div className="container px-0 overflow-visible vh-100">
-      <InfiniteScroll
-        className="pr-0 mr-0"
-        dataLength={items.length}
-        next={loadMoreItems}
-        hasMore={hasMoreItems}
-        loader={<h4>Loading...</h4>}
-      >
-        {items.map((item, index) => (
-          <StatusItem
-            key={index}
-            item={item}
-          />
-        ))}
-      </InfiniteScroll>
-    </div>
-  );
+			setHasMoreItems(hasMore);
+			setLastItem(newItems[newItems.length - 1]);
+			addItems(newItems);
+			setChangedDisplayedUser(false)
+		} catch (error) {
+			displayErrorMessage(
+				`Failed to load ${props.itemDescription} items because of exception: ${error}`
+			);
+		}
+	};
+
+	return (
+		<div className="container px-0 overflow-visible vh-100">
+			<InfiniteScroll
+				className="pr-0 mr-0"
+				dataLength={items.length}
+				next={loadMoreItems}
+				hasMore={hasMoreItems}
+				loader={<h4>Loading...</h4>}
+			>
+				{items.map((item, index) => (
+					<StatusItem
+						key={index}
+						item={item}
+					/>
+				))}
+			</InfiniteScroll>
+		</div>
+	);
 };
 
 export default StatusItemScroller;
