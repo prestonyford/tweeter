254551e0ecee49aadb8c8e61344297d3ebc7b8d3
Preston Ford <preston.y.ford@gmail.com>
2025-02-08 20:58:30 -0700

    refactor UserItemScroller to MVP

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -1,135 +1,116 @@
 import "./App.css";
 import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
+	BrowserRouter,
+	Navigate,
+	Route,
+	Routes,
+	useLocation,
 } from "react-router-dom";
 import Login from "./components/authentication/login/Login";
 import Register from "./components/authentication/register/Register";
 import MainLayout from "./components/mainLayout/MainLayout";
 import Toaster from "./components/toaster/Toaster";
-import { AuthToken, User, FakeData, Status } from "tweeter-shared";
+import { AuthToken, FakeData, Status } from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
 import StatusItemScroller from "./components/mainLayout/StatusItemScroller";
 import useUserInfo from "./components/userInfo/userInfoHook";
+import { FolloweePresenter } from "./presenters/FolloweePresenter";
+import { UserItemView } from "./presenters/UserItemPresenter";
+import { FollowerPresenter } from "./presenters/FollowerPresenter";
 
 const App = () => {
-  const { currentUser, authToken } = useUserInfo();
+	const { currentUser, authToken } = useUserInfo();
 
-  const isAuthenticated = (): boolean => {
-    return !!currentUser && !!authToken;
-  };
+	const isAuthenticated = (): boolean => {
+		return !!currentUser && !!authToken;
+	};
 
-  return (
-    <div>
-      <Toaster position="top-right" />
-      <BrowserRouter>
-        {isAuthenticated() ? (
-          <AuthenticatedRoutes />
-        ) : (
-          <UnauthenticatedRoutes />
-        )}
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<Toaster position="top-right" />
+			<BrowserRouter>
+				{isAuthenticated() ? (
+					<AuthenticatedRoutes />
+				) : (
+					<UnauthenticatedRoutes />
+				)}
+			</BrowserRouter>
+		</div>
+	);
 };
 
 const AuthenticatedRoutes = () => {
-  const loadMoreFeedItems = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: Status | null
-  ): Promise<[Status[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-  };
+	const loadMoreFeedItems = async (
+		authToken: AuthToken,
+		userAlias: string,
+		pageSize: number,
+		lastItem: Status | null
+	): Promise<[Status[], boolean]> => {
+		// TODO: Replace with the result of calling server
+		return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+	};
 
-  const loadMoreStoryItems = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: Status | null
-  ): Promise<[Status[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-  };
+	const loadMoreStoryItems = async (
+		authToken: AuthToken,
+		userAlias: string,
+		pageSize: number,
+		lastItem: Status | null
+	): Promise<[Status[], boolean]> => {
+		// TODO: Replace with the result of calling server
+		return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+	};
 
-  const loadMoreFollowers = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: User | null
-  ): Promise<[User[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-  };
-
-  const loadMoreFollowees = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: User | null
-  ): Promise<[User[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-  };
-
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Navigate to="/feed" />} />
-        <Route
-          path="feed"
-          element={
-            <StatusItemScroller loadItems={loadMoreFeedItems} itemDescription="feed" />
-          }
-        />
-        <Route
-          path="story"
-          element={
-            <StatusItemScroller loadItems={loadMoreStoryItems} itemDescription="story" />
-          }
-        />
-        <Route
-          path="followees"
-          element={
-            <UserItemScroller
-              key={1}
-              loadItems={loadMoreFollowees}
-                itemDescription="followees"
-            />
-          }
-        />
-        <Route
-          path="followers"
-          element={
-            <UserItemScroller
-              key={2} 
-              loadItems={loadMoreFollowers}
-                itemDescription="followers"
-            />
-          }
-        />
-        <Route path="logout" element={<Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/feed" />} />
-      </Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route element={<MainLayout />}>
+				<Route index element={<Navigate to="/feed" />} />
+				<Route
+					path="feed"
+					element={
+						<StatusItemScroller loadItems={loadMoreFeedItems} itemDescription="feed" />
+					}
+				/>
+				<Route
+					path="story"
+					element={
+						<StatusItemScroller loadItems={loadMoreStoryItems} itemDescription="story" />
+					}
+				/>
+				<Route
+					path="followees"
+					element={
+						<UserItemScroller
+							key={1}
+							presenterGenerator={(view: UserItemView) => new FolloweePresenter(view)}
+						/>
+					}
+				/>
+				<Route
+					path="followers"
+					element={
+						<UserItemScroller
+							key={2}
+							presenterGenerator={(view: UserItemView) => new FollowerPresenter(view)}
+						/>
+					}
+				/>
+				<Route path="logout" element={<Navigate to="/login" />} />
+				<Route path="*" element={<Navigate to="/feed" />} />
+			</Route>
+		</Routes>
+	);
 };
 
 const UnauthenticatedRoutes = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<Login originalUrl={location.pathname} />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="*" element={<Login originalUrl={location.pathname} />} />
+		</Routes>
+	);
 };
 
 export default App;
--- tweeter-web/src/components/mainLayout/UserItemScroller.tsx
+++ tweeter-web/src/components/mainLayout/UserItemScroller.tsx
@@ -4,99 +4,79 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import UserItem from "../userItem/UserItem";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "../userInfo/userInfoHook";
-
-export const PAGE_SIZE = 10;
+import { UserItemPresenter, UserItemView } from "../../presenters/UserItemPresenter";
 
 interface Props {
-  loadItems: (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: User | null
-  ) => Promise<[User[], boolean]>;
-  itemDescription: string;
+	presenterGenerator: (view: UserItemView) => UserItemPresenter
 }
 
 const UserItemScroller = (props: Props) => {
-  const { displayErrorMessage } = useToastListener();
-  const [items, setItems] = useState<User[]>([]);
-  const [newItems, setNewItems] = useState<User[]>([]);
-  const [hasMoreItems, setHasMoreItems] = useState(true);
-  const [lastItem, setLastItem] = useState<User | null>(null);
-  const [changedDisplayedUser, setChangedDisplayedUser] = useState(true);
+	const { displayErrorMessage } = useToastListener();
+	const [items, setItems] = useState<User[]>([]);
+	const [newItems, setNewItems] = useState<User[]>([]);
+	const [changedDisplayedUser, setChangedDisplayedUser] = useState(true);
 
-  const addItems = (newItems: User[]) =>
-    setNewItems(newItems);
+	const { displayedUser, authToken } = useUserInfo();
 
-  const { displayedUser, authToken } = useUserInfo();
+	// Initialize the component whenever the displayed user changes
+	useEffect(() => {
+		reset();
+	}, [displayedUser]);
 
-  // Initialize the component whenever the displayed user changes
-  useEffect(() => {
-    reset();
-  }, [displayedUser]);
+	// Load initial items whenever the displayed user changes. Done in a separate useEffect hook so the changes from reset will be visible.
+	useEffect(() => {
+		if (changedDisplayedUser) {
+			loadMoreItems();
+		}
+	}, [changedDisplayedUser]);
 
-  // Load initial items whenever the displayed user changes. Done in a separate useEffect hook so the changes from reset will be visible.
-  useEffect(() => {
-    if(changedDisplayedUser) {
-      loadMoreItems();
-    }
-  }, [changedDisplayedUser]);
+	// Add new items whenever there are new items to add
+	useEffect(() => {
+		if (newItems) {
+			setItems([...items, ...newItems]);
+		}
+	}, [newItems])
 
-  // Add new items whenever there are new items to add
-  useEffect(() => {
-    if(newItems) {
-      setItems([...items, ...newItems]);
-    }
-  }, [newItems])
+	const reset = async () => {
+		setItems([]);
+		setNewItems([]);
+		setChangedDisplayedUser(true);
 
-  const reset = async () => {
-    setItems([]);
-    setNewItems([]);
-    setLastItem(null);
-    setHasMoreItems(true);
-    setChangedDisplayedUser(true);
-  }
+		presenter.reset();
+	}
 
-  const loadMoreItems = async () => {
-    try {
-      const [newItems, hasMore] = await props.loadItems(
-        authToken!,
-        displayedUser!.alias,
-        PAGE_SIZE,
-        lastItem
-      );
+	const listener: UserItemView = {
+		addItems: (newItems: User[]) => setNewItems(newItems),
+		displayErrorMessage
+	}
 
-      setHasMoreItems(hasMore);
-      setLastItem(newItems[newItems.length - 1]);
-      addItems(newItems);
-      setChangedDisplayedUser(false)
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to load ${props.itemDescription} because of exception: ${error}`
-      );
-    }
-  };
+	const [presenter] = useState(props.presenterGenerator(listener));
 
-  return (
-    <div className="container px-0 overflow-visible vh-100">
-      <InfiniteScroll
-        className="pr-0 mr-0"
-        dataLength={items.length}
-        next={loadMoreItems}
-        hasMore={hasMoreItems}
-        loader={<h4>Loading...</h4>}
-      >
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="row mb-3 mx-0 px-0 border rounded bg-white"
-          >
-            <UserItem value={item} />
-          </div>
-        ))}
-      </InfiniteScroll>
-    </div>
-  );
+	const loadMoreItems = async () => {
+		presenter.loadMoreItems(authToken!, displayedUser!.alias);
+		setChangedDisplayedUser(false);
+	}
+
+	return (
+		<div className="container px-0 overflow-visible vh-100">
+			<InfiniteScroll
+				className="pr-0 mr-0"
+				dataLength={items.length}
+				next={loadMoreItems}
+				hasMore={presenter.hasMoreItems}
+				loader={<h4>Loading...</h4>}
+			>
+				{items.map((item, index) => (
+					<div
+						key={index}
+						className="row mb-3 mx-0 px-0 border rounded bg-white"
+					>
+						<UserItem value={item} />
+					</div>
+				))}
+			</InfiniteScroll>
+		</div>
+	);
 };
 
 export default UserItemScroller;
